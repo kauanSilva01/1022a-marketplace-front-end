ae1e6e9fc86ce14be0674149fb12004f2e71737b
Vinizerahsz <116743023+Vinizerahsz@users.noreply.github.com>
2024-10-14 10:18:05 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+type ProdutoType = {
+  id:number,
+  nome:string,
+  preco:string, 
+  descricao:string,
+  imagem:string
+}
 
+function App() {
+  const [nome, setNome] = useState("")
+  const [produtos, setProdutos] = useState<ProdutoType []>([])
+  useEffect(()=>{
+    setNome("Biego Darros")
+  },[])
+  fetch("https://one022a-marketplace-bggt.onrender.com/produtos")
+  .then(resposta => resposta.json())
+  .then(dados => setProdutos(dados))
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>{nome}</h1>
+      <div className="produtos-container">
+        {
+          produtos.map(produto =>{
+            return(
+          <div className='produto-item'>
+            <h1>{produto.nome}</h1>
+            <img src={produto.imagem} alt="Imagem do celular" />
+            <p>{produto.preco}</p>
+            <p>{produto.descricao}</p>
+          </div>
+            )
+          })
+        }
+       
       </div>
-      <h1>Guilherme Figueiredo Terenciani</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
