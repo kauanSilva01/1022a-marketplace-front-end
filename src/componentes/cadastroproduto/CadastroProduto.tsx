6380086443511fbe1d85a66925448de0bb99d2c7
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-11-05 11:47:18 -0400

    Update CadastroProduto.tsx

--- src/componentes/cadastroproduto/CadastroProduto.tsx
+++ src/componentes/cadastroproduto/CadastroProduto.tsx
@@ -1,10 +1,24 @@
+import {  ChangeEvent, FormEvent, useState } from "react"
+
 function CadastroProduto(){
+    const [id,setId] = useState("")
+    function handleForm(event:FormEvent){
+        event.preventDefault()
+        console.log("Cliquei em cadastrar")
+        //Pegar os valores dos campos
+        //Mandar pelo fetch para o Back-End
+    }
+    function handleId(event:ChangeEvent<HTMLInputElement>){
+        setId(event.target.value)
+    }
+
+    }
     return(
         <>
             <h1>Meu Componente de Cadastro de Produtos</h1>
-            <form>
+            <form onSubmit={handleForm}>
                 <div>
-                    <input placeholder="Id" type="text" name="id" id="id" />
+                    <input placeholder="Id" type="text" name="id" id="id" onClick={handleId} />
                 </div>
                 <div>
                     <input placeholder="Nome" type="text" name="nome" id="nome" />
